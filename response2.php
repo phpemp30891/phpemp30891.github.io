b654d159654e6f1a9a53f91e03e908a30c7db999
Navdeep Patel <php.emp30891@gmail.com>
2023-11-30 01:27:29 +0530

    Update response2.php

--- response2.php
+++ response2.php
@@ -1,4 +1,4 @@
 <?php
-$red3="https://www.loan-broker.com/index.php?email=".$_REQUEST['email']."&cellPhone=".$_REQUEST['phone']."&zip=".$_REQUEST['zip']."&firstName=".$_REQUEST['fname']."&lastName=".$_REQUEST['lname']."&loanAmount=".$_REQUEST['loan'];	
+$red3="https://www.loan-broker.com/application.php?email=".$_REQUEST['email']."&cellPhone=".$_REQUEST['phone']."&zip=".$_REQUEST['zip']."&firstName=".$_REQUEST['fname']."&lastName=".$_REQUEST['lname']."&loanAmount=".$_REQUEST['loan'];	
 header('location:'.$red3);
 ?>
